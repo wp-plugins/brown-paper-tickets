e52f9fed12afba5660d41aa5b8c1261920a14fe3
Chandler Blum <Chandler Blum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 19:12:00 +0000

    Tagging version 0.1.3
    git-svn-id: https://plugins.svn.wordpress.org/brown-paper-tickets/tags/0.1.3@986734 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/assets/js/bpt-calendar.js
@@ -0,0 +1,197 @@
+(function($) {
+    'use strict';
+    var BptCalendar;
+
+    BptCalendar = function(calendarOptions) {
+        var bptCalendar = this,
+            events = [],
+            currentEvents = [],
+            calendar,
+            calendarContainer = calendarOptions.calendarContainer,
+            calendarTemplate = $('#bpt-calendar-widget-calendar-template').html(),
+            eventListContainer = calendarOptions.eventListContainer,
+            eventListTemplate = '#bpt-calendar-widget-event-view-template',
+            showUpcoming = calendarOptions.showUpcoming === 'true' ? true : false,
+            eventList,
+            getEvents,
+            setEvents,
+            displayEvents;
+
+        this.events = events;
+        this.currentEvents = currentEvents;
+
+        /**
+         * This is the Ractive Template that displays the 
+         * event details.
+         */
+        eventList = new Ractive({
+            el: eventListContainer,
+            template: eventListTemplate,
+            data: {
+                showUpcoming: showUpcoming, // If show upcoming is enabled, show upcoming dates.
+                currentEvents: bptCalendar.events,
+                formatDate: function formatDate(newFormat, date) {
+                    var singleDate = moment(date, 'YYYY-MM-DD');
+                    return singleDate.format(newFormat);
+                },
+                formatTime: function formatTime(newFormat, time) {
+                    var singleTime = moment(time, 'H:mm');
+                    return singleTime.format(newFormat);
+                },
+                unescapeHTML: function unescapeHTML(html) {
+                    return _.unescape(html);
+                }
+            }
+        });
+
+        /**
+         * This is the calendar itself.
+         */
+        calendar = $(calendarOptions.calendarContainer).clndr({
+            template: calendarTemplate,
+            targets: {
+                nextButton: 'bpt-calendar-widget-controls-next-button',
+                previousButton: 'bpt-calendar-widget-controls-previous-button',
+                nextYearButton: 'clndr-next-year-button',
+                previousYearButton: 'clndr-previous-year-button',
+                todayButton: 'clndr-today-button',
+                day: 'day',
+                empty: 'empty'
+            },
+            clickEvents: {
+                click: function(target) {
+                    displayEvents(target);
+                },
+                onMonthChange: function(month) {
+                    displayEvents(month);
+                }
+            },
+            ready: function() {
+                console.log(calendar);
+            }
+        });
+
+        this.getEvents = function() {
+
+            var ajaxData = {
+                action: 'bpt_get_calendar_events',
+                bptNonce: calendarOptions.bptNonce,
+                widgetID: calendarOptions.widgetID,
+            };
+
+            if (calendarOptions.clientID) {
+                ajaxData.clientID = calendarOptions.clientID;
+            }
+
+            $.ajax(
+            calendarOptions.ajaxurl,
+            {
+                type: 'POST',
+                data: ajaxData,
+                accepts: 'json',
+                dataType: 'json'
+
+            }).done(function(data) {
+                setEvents(data);
+
+                calendar.options.ready();
+
+            }).fail(function(data) {
+
+                setEvents(data);
+
+            });
+        };
+        
+        setEvents = function(events) {
+
+            bptCalendar.events = events;
+            calendar.setEvents(events);
+            displayEvents();
+        };
+
+        displayEvents = function(target) {
+            var dayEvents = [];
+            
+            if (target && target.element) {
+                var container = document.querySelector(calendarContainer);
+                var previousSelected =container.querySelectorAll('.bpt-calendar-selected-day');
+
+                for (var i = 0; i < previousSelected.length; i++) {
+                    previousSelected[i].classList.remove('bpt-calendar-selected-day');
+                }
+
+                target.element.classList.add('bpt-calendar-selected-day');
+            }
+
+            if (target && !target._isAMomentObject) {
+                dayEvents = target.events;
+            }
+
+            if (showUpcoming && dayEvents.length === 0) {
+
+                if (calendar.eventsThisMonth.length !== 0) {
+
+                    eventList.set({
+                        date: false,
+                        eventsThisMonth: true,
+                        showUpcoming: false,
+                        currentEvents: calendar.eventsThisMonth,
+                    });
+
+                    return;
+
+                } else {
+
+                    eventList.set({
+                        date: false,
+                        eventsThisMonth: false,
+                        showUpcoming: true,
+                        currentEvents: bptCalendar.events.slice(0, 5),
+                    });
+
+                    return;
+                }
+
+            }
+
+            if (dayEvents.length > 0) {
+
+                eventList.set({
+                    date: dayEvents[0].date,
+                    eventsThisMonth: false,
+                    showUpcoming: false,
+                    currentEvents: dayEvents
+                });
+
+                return;
+            }
+
+            eventList.set({
+                date: false,
+                eventsThisMonth: false,
+                showUpcoming: false,
+                currentEvents: false
+            });
+        };
+    };
+
+    $(document).ready(function() {
+
+        if (bptCalendarWidgetShortcodeAjax) {
+
+            var bptCalendarShortcode = new BptCalendar(bptCalendarWidgetShortcodeAjax);
+            
+            bptCalendarShortcode.getEvents();
+
+        }
+
+        if (bptCalendarWidgetAjax) {
+            
+            var bptCalendarWidget = new BptCalendar(bptCalendarWidgetAjax);
+            
+            bptCalendarWidget.getEvents();
+        }
+    });
+
+})(jQuery);

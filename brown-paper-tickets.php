08657a7b468fad6f1ee8c37cd4ddccd887ad5161
Chandler Blum <Chandler Blum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-15 23:19:57 +0000

    Fixing version tag.
    git-svn-id: https://plugins.svn.wordpress.org/brown-paper-tickets/trunk@1135771 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- brown-paper-tickets.php
+++ brown-paper-tickets.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Brown Paper Tickets
  * Plugin URI:        http://www.brownpapertickets.com
  * Description:       Display your Brown Paper Tickets events using convenient shortcodes and widgets.
- * Version:           0.4.1
+ * Version:           0.5.0
  * Author:            Brown Paper Tickets
  * Author URI:        http://www.brownpapertickets.com
  * Text Domain:       brown-paper-tickets-locale
